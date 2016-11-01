51c518cb71956d745c96aa8408e0191f4dcbbd6e
Chennaiah1990 <birruchennaiah1990@gmail.com>
2016-11-01 07:20:23 -0700

    struct commit

---
+++ vikas-app/src/main/java/com/vikassoft/app/App.java
@@ -0,0 +1,13 @@
+package com.vikassoft.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "hai vikas welcome maven");
+    }
+}
